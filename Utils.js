6f3530f29ed9c35a0aff3d80f636d5f50b73578a
ivLis <me@ivl.is>
2025-11-25 01:19:53 +0900

    Fix scroll when full screen extension is activate

--- Utils.js
+++ Utils.js
@@ -834,7 +834,7 @@ const Utils = {
   /**
    * Current version of the lyrics-plus app
    */
-  currentVersion: "2.1.4",
+  currentVersion: "2.1.5",
 
   /**
    * Check for updates from remote repository
